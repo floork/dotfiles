aa37607a56b353f511d1cb7c08256b6030c8608f
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-10-04 14:25:44 +0200

    add(bash): bash now in here too :)

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ ln -s $SCRIPT_DIR/zsh $HOME/.config/zsh
 ln -s $SCRIPT_DIR/zsh/zshrc $HOME/.zshrc
 ln -s $SCRIPT_DIR/kitty $HOME/.config/kitty
 ln -s $SCRIPT_DIR/nvim $HOME/.config/nvim
+ln -s $SCRIPT_DIR/bashrc $HOME/.bashrc
 

42a5a9ec97899bdfe7a123f1fa9601fbfc02bd22
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-10-05 11:16:55 +0200

    add(nvim): keybinds for harpoon

--- nvim/lua/configs/plugins/harpoon.lua
+++ nvim/lua/configs/plugins/harpoon.lua
@@ -13,12 +13,24 @@ return {
       "<cmd>lua require('harpoon.mark').add_file()<cr>",
       { desc = "Mark file with harpoon" }
     )
+    keymap.set(
+      "n",
+      "<leader>hr",
+      "<cmd>lua require('harpoon.mark').remove_file()<cr>",
+      { desc = "Remove file from harpoon" }
+    )
     keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
     keymap.set(
       "n",
       "<leader>hp",
       "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
       { desc = "Go to previous harpoon mark" }
+    )
+    keymap.set(
+      "n",
+      "<leader>ha",
+      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+      { desc = "Toggle harpoon quick menu" }
     )
   end,
 }

f613baf7b11326ea2f44acf33d8e4448e43e9c53
floork <mail@floork.dev>
2023-10-23 18:19:08 +0200

    add: major changes

--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
-#!/bin/bash
-#
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/usr/bin/env bash
 
-ln -s $SCRIPT_DIR/zsh $HOME/.config/zsh
-ln -s $SCRIPT_DIR/zsh/zshrc $HOME/.zshrc
-ln -s $SCRIPT_DIR/kitty $HOME/.config/kitty
-ln -s $SCRIPT_DIR/nvim $HOME/.config/nvim
-ln -s $SCRIPT_DIR/bashrc $HOME/.bashrc
-ln -s $SCRIPT_DIR/gitconfig $HOME/.gitconfig
-ln -s $SCRIPT_DIR/starship.toml $HOME/.config/starship.toml
-ln -s $SCRIPT_DIR/neofetch $HOME/.config/neofetch
-ln -s $SCRIPT_DIR/fish $HOME/.config/fish
+# Get the directory where the script is located
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Define an associative array to store the symlink targets.
+declare -A symlinks
+symlinks[".gitconfig"]="$script_dir/gitconfig"
+symlinks[".bashrc"]="$script_dir/bashrc"
+symlinks[".zshrc"]="$script_dir/zsh/zshrc"
+symlinks[".config/dconf"]="$script_dir/dconf"
+symlinks[".config/fish"]="$script_dir/fish"
+symlinks[".config/gh"]="$script_dir/gh"
+symlinks[".config/gtk-3.0"]="$script_dir/gtk-3.0"
+symlinks[".config/hypr"]="$script_dir/hypr"
+symlinks[".config/kitty"]="$script_dir/kitty"
+symlinks[".config/neofetch"]="$script_dir/neofetch"
+symlinks[".config/nvim"]="$script_dir/nvim"
+symlinks[".config/starship.toml"]="$script_dir/starship.toml"
+symlinks[".config/waybar"]="$script_dir/waybar"
+symlinks[".config/wofi"]="$script_dir/wofi"
+symlinks[".config/xsettingsd"]="$script_dir/xsettingsd"
+symlinks[".config/zsh"]="$script_dir/zsh"
+
+# Iterate over the associative array and create symlinks.
+for file in "${!symlinks[@]}"; do
+  if [ -e "$HOME/$file" ]; then
+    echo "Symlink for $file already exists."
+  else
+    ln -s "${symlinks[$file]}" "$HOME/$file"
+    echo "Symlink for $file created."
+  fi
+done
 

c38d9932b23b9e296f32e02d8ea743c3c374e8ec
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-11-09 12:46:36 +0100

    add(nvim): new file browser

--- nvim/lua/configs/core/options.lua
+++ nvim/lua/configs/core/options.lua
@@ -42,3 +42,6 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- set scope
+vim.cmd("autocmd BufEnter * if expand('#') != '' | execute 'cd ' .. fnameescape(expand('%:p:h')) | endif")

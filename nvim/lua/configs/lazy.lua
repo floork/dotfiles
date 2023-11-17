f1d019b441da49b32e02e72bc445477b78ad6fdd
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-11-17 12:13:43 +0100

    add(nvim): lazy keymap

--- nvim/lua/configs/lazy.lua
+++ nvim/lua/configs/lazy.lua
@@ -1,24 +1,33 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{
-    import = "configs.plugins"
+-- keymap
+local keymap = vim.keymap
+keymap.set("n", "<leader>ll", ":Lazy<CR>", { noremap = true, silent = true })
+
+require("lazy").setup({ {
+  import = "configs.plugins",
 }, {
-    import = "configs.plugins.lsp"
-}}, {
-    install = {
-        colorscheme = {"nightfly"}
-    },
-    checker = {
-        enabled = true,
-        notify = false
-    },
-    change_detection = {
-        notify = false
-    }
+  import = "configs.plugins.lsp",
+} }, {
+  install = {
+    colorscheme = { "nightfly" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })

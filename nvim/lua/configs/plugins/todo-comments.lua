3a3eec4636682668e3324862ec3ab292b4ae7e0e
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-11-17 14:38:17 +0100

    add(nvim): more todo comment keybinds

--- nvim/lua/configs/plugins/todo-comments.lua
+++ nvim/lua/configs/plugins/todo-comments.lua
@@ -12,6 +12,13 @@ return {
     local keymap = vim.keymap
 
     local possible_comment_types = "FIX|TODO|HACK|WARN|PERF|NOTE|TEST"
-    keymap.set("n", "<leader>;;", "<cmd>:TodoTelescope<cr>", { desc = possible_comment_types })
+    keymap.set("n", "<leader>;;", "<cmd>TodoTelescope<cr>", { desc = possible_comment_types })
+    keymap.set("n", "<leader>;f", "<cmd>TodoTelescope keywords=FIX<cr>", { desc = "Fix" })
+    keymap.set("n", "<leader>;t", "<cmd>TodoTelescope keywords=TODO<cr>", { desc = "Todo" })
+    keymap.set("n", "<leader>;h", "<cmd>TodoTelescope keywords=HACK<cr>", { desc = "Hack" })
+    keymap.set("n", "<leader>;w", "<cmd>TodoTelescope keywords=WARN<cr>", { desc = "Warn" })
+    keymap.set("n", "<leader>;p", "<cmd>TodoTelescope keywords=PERF<cr>", { desc = "Perf" })
+    keymap.set("n", "<leader>;n", "<cmd>TodoTelescope keywords=NOTE<cr>", { desc = "Note" })
+    keymap.set("n", "<leader>;l", "<cmd>TodoTelescope keywords=TEST<cr>", { desc = "Test" })
   end,
 }

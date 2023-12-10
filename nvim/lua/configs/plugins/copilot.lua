70b395172a8f142c5e3e31145a92a4cdd834af83
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-12-10 18:19:44 +0100

    add(nvim): context.vim + better copilot plugin

--- nvim/lua/configs/plugins/copilot.lua
+++ nvim/lua/configs/plugins/copilot.lua
@@ -1,3 +1,19 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  cmd = { "Copilot" },
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+        },
+      },
+    })
+
+    -- keymap
+    local keymap = vim.keymap
+    keymap.set("i", "<C-P>", "<cmd> Copilot panel <CR>")
+  end,
 }

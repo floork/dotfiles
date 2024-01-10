bf68d2bd69361a10d8286a3ab3de0fad6eeed1ff
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-10 15:44:15 +0100

    add(wezterm): white bg funtion

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,18 @@
 local wezterm = require("wezterm")
+
+local function whiteBG()
+	return {
+		orientation = "Vertical",
+		colors = { "#FFFFFF" },
+		interpolation = "Linear",
+		blend = "Rgb",
+	}
+end
+
 local config = {
 	window_background_opacity = 0.9,
 	font = wezterm.font("Fira Code"),
+	-- window_background_gradient = whiteBG(), -- uncomment to enable
 }
 
 config.color_scheme = "Apprentice (base16)"

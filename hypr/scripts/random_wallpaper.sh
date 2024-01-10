1c00d45ec33f8973879c20435e983c7ca435cd93
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-10 12:23:42 +0100

    fix: files from .git dir as bg not possible anymore

--- hypr/scripts/random_wallpaper.sh
+++ hypr/scripts/random_wallpaper.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 wallpapers_dir=~/.config/wallpapers/
+exclude_dir1=~/.config/wallpapers/.git/
+exclude_dir2=~/.config/wallpapers/anime/.git/
+
 if [ $(ps aux | grep swaybg | grep -v grep | wc -l) -gt 0 ]; then
     killall swaybg
 fi
 
-swaybg -i "$(find "$wallpapers_dir" -type f | shuf -n1)" -m fill &
+swaybg -i "$(find "$wallpapers_dir" -type f -not -path "$exclude_dir1*" -not -path "$exclude_dir2*" | shuf -n1)" -m fill &
 OLD_PID=$!
 
 while true; do
     sleep 600
-    swaybg -i "$(find "$wallpapers_dir" -type f | shuf -n1)" -m fill &
+    swaybg -i "$(find "$wallpapers_dir" -type f -not -path "$exclude_dir1*" -not -path "$exclude_dir2*" | shuf -n1)" -m fill &
     NEXT_PID=$!
     sleep 5
     kill $OLD_PID
     OLD_PID=$NEXT_PID
 done
-

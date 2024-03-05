2c1559ac4a5c0d697807565f76857875dfc08eb6
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-05 10:50:49 +0100

    add(install): better format

--- install.sh
+++ install.sh
@@ -1,123 +1,147 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+function create_symlinks() {
+	declare -A symlinks=(
+		# SHELL
+		# # bash
+		[".bashrc"]="$script_dir/bash/bashrc"
+		# # fish
+		[".config/fish"]="$script_dir/fish"
+		# # zsh
+		[".config/zsh"]="$script_dir/zsh"
+		[".zshrc"]="$script_dir/zsh/zshrc"
+		# # shared resources
+		[".aliasrc"]="$script_dir/shell/aliasrc"
+		[".functionsrc"]="$script_dir/shell/functionsrc"
 
-create_symlinks() {
-    declare -A symlinks=(
-    # SHELL
-    # # bash
-    [".bashrc"]="$script_dir/bash/bashrc"
-    # # fish
-    [".config/fish"]="$script_dir/fish"
-    # # zsh
-    [".config/zsh"]="$script_dir/zsh"
-    [".zshrc"]="$script_dir/zsh/zshrc"
-    # # shared resources
-    [".aliasrc"]="$script_dir/shell/aliasrc"
-    [".functionsrc"]="$script_dir/shell/functionsrc"
+		# GIT
+		[".gitconfig"]="$script_dir/git/gitconfig"
+		[".config/gh"]="$script_dir/gh"
 
-    # GIT
-    [".gitconfig"]="$script_dir/git/gitconfig" 
-    [".config/gh"]="$script_dir/gh"
+		# CLI
+		# # tmux
+		[".tmux.conf"]="$script_dir/tmux/tmux.conf"
+		# # neofetch
+		[".config/neofetch"]="$script_dir/neofetch"
+		# # starship
+		[".config/starship.toml"]="$script_dir/starship/starship.toml"
 
-    # CLI
-    # # tmux
-    [".tmux.conf"]="$script_dir/tmux/tmux.conf"
-    # # neofetch
-    [".config/neofetch"]="$script_dir/neofetch"
-    # # starship
-    [".config/starship.toml"]="$script_dir/starship/starship.toml"
+		# Desktop applications
+		[".config/dunst"]="$script_dir/dunst"
+		[".config/hypr"]="$script_dir/hypr"
+		[".config/Thunar"]="$script_dir/Thunar"
+		[".config/waybar"]="$script_dir/waybar"
+		[".config/wofi"]="$script_dir/wofi"
+		# # terminal
+		[".config/warp-terminal"]="$script_dir/warp-terminal"
+		[".config/wezterm"]="$script_dir/wezterm"
+	)
 
-    # Desktop applications
-    [".config/dunst"]="$script_dir/dunst"
-    [".config/hypr"]="$script_dir/hypr"
-    [".config/Thunar"]="$script_dir/Thunar"
-    [".config/waybar"]="$script_dir/waybar"
-    [".config/wofi"]="$script_dir/wofi"
-    # # terminal
-    [".config/warp-terminal"]="$script_dir/warp-terminal"
-    [".config/wezterm"]="$script_dir/wezterm"
-    )
+	create_symlink() {
+		local source=$1
+		local target=$2
 
-    create_symlink() {
-        local source=$1
-        local target=$2
+		if [ -e "$target" ]; then
+			echo "Symlink for $source already exists."
+		else
+			ln -s "$source" "$target" || {
+				echo "Failed to create symlink for $source"
+				exit 1
+			}
+			echo "Symlink for $source created."
+		fi
+	}
 
-        if [ -e "$target" ]; then
-            echo "Symlink for $source already exists."
-        else
-            ln -s "$source" "$target" || { echo "Failed to create symlink for $source"; exit 1; }
-            echo "Symlink for $source created."
-        fi
-    }
-
-    for source in "${!symlinks[@]}"; do
-        create_symlink "${symlinks[$source]}" "$HOME/$source"
-    done
+	for source in "${!symlinks[@]}"; do
+		create_symlink "${symlinks[$source]}" "$HOME/$source"
+	done
 }
 
-install_tmux_plugin_manager() {
-    tpm_dir="$HOME/.tmux/plugins/tpm"
-    if [ ! -d "$tpm_dir" ]; then
-        git clone https://github.com/tmux-plugins/tpm "$tpm_dir" || { echo "Failed to install TMUX Plugin Manager"; exit 1; }
-        echo "TMUX Plugin Manager installed."
-    fi
+function install_tmux_plugin_manager() {
+	tpm_dir="$HOME/.tmux/plugins/tpm"
+	if [ ! -d "$tpm_dir" ]; then
+		git clone https://github.com/tmux-plugins/tpm "$tpm_dir" || {
+			echo "Failed to install TMUX Plugin Manager"
+			exit 1
+		}
+		echo "TMUX Plugin Manager installed."
+	fi
 }
 
-install_wallpapers() {
-    WALLPAPERS_DIR="$HOME/.config/wallpapers"
-    echo "Do you want to install wallpapers? (yes/No): "
-    read -r ANSWER
-    if [ "${ANSWER,,}" == "yes" ] || [ "${ANSWER,,}" == "y" ]; then
-        if [ -d "$WALLPAPERS_DIR" ]; then
-            rm -rf "$WALLPAPERS_DIR"
-        fi
-        git clone https://github.com/floork/wallpapers.git "$WALLPAPERS_DIR" || { echo "Failed to install wallpapers"; exit 1; }
-        echo "Wallpapers installed successfully."
-    fi
+function install_wallpapers() {
+	WALLPAPERS_DIR="$HOME/.config/wallpapers"
+	echo "Do you want to install wallpapers? (yes/No): "
+	read -r ANSWER
+	if [ "${ANSWER,,}" == "yes" ] || [ "${ANSWER,,}" == "y" ]; then
+		if [ -d "$WALLPAPERS_DIR" ]; then
+			rm -rf "$WALLPAPERS_DIR"
+		fi
+		git clone https://github.com/floork/wallpapers.git "$WALLPAPERS_DIR" || {
+			echo "Failed to install wallpapers"
+			exit 1
+		}
+		echo "Wallpapers installed successfully."
+	fi
 }
 
-update_git_repositories() {
-    WALLPAPERS_DIR="$HOME/.config/wallpapers"
-    echo "Updating wallpapers repository..."
-    if [ -d "$WALLPAPERS_DIR" ]; then
-        cd "$WALLPAPERS_DIR" || exit
-        git pull --recurse-submodules || { echo "Failed to update wallpapers repository"; exit 1; }
-        cd "$script_dir" || exit
-    fi
+function update_git_repositories() {
+	WALLPAPERS_DIR="$HOME/.config/wallpapers"
+	echo "Updating wallpapers repository..."
+	if [ -d "$WALLPAPERS_DIR" ]; then
+		cd "$WALLPAPERS_DIR" || exit
+		git pull --recurse-submodules || {
+			echo "Failed to update wallpapers repository"
+			exit 1
+		}
+		cd "$script_dir" || exit
+	fi
 
-    NVIM_CONFIG_DIR="$HOME/.config/nvim"
-    echo "Updating nvim configuration..."
-    if [ -d "$NVIM_CONFIG_DIR" ]; then
-        cd "$NVIM_CONFIG_DIR" || exit
-        git pull || { echo "Failed to update nvim configuration"; exit 1; }
-        cd "$script_dir" || exit
-    else
-        git clone https://github.com/floork/nvim.git "$NVIM_CONFIG_DIR" || { echo "Failed to clone nvim configuration"; exit 1; }
-    fi
+	NVIM_CONFIG_DIR="$HOME/.config/nvim"
+	echo "Updating nvim configuration..."
+	if [ -d "$NVIM_CONFIG_DIR" ]; then
+		cd "$NVIM_CONFIG_DIR" || exit
+		git pull || {
+			echo "Failed to update nvim configuration"
+			exit 1
+		}
+		cd "$script_dir" || exit
+	else
+		git clone https://github.com/floork/nvim.git "$NVIM_CONFIG_DIR" || {
+			echo "Failed to clone nvim configuration"
+			exit 1
+		}
+	fi
 
-    if [ ! -e "$script_dir/nvim" ]; then
-        ln -s "$NVIM_CONFIG_DIR" "$script_dir/nvim" || { echo "Failed to create symlink for nvim"; exit 1; }
-    fi
+	if [ ! -e "$script_dir/nvim" ]; then
+		ln -s "$NVIM_CONFIG_DIR" "$script_dir/nvim" || {
+			echo "Failed to create symlink for nvim"
+			exit 1
+		}
+	fi
 }
 
-update_user_bin_directory() {
-    BIN_DIR="$HOME/.local/bin"
-    mkdir -p "$BIN_DIR"
+function update_user_bin_directory() {
+	BIN_DIR="$HOME/.local/bin"
+	mkdir -p "$BIN_DIR"
 
-    for file in "$script_dir/bin"/*; do
-        ln -sf "$file" "$BIN_DIR/" || { echo "Failed to update user bin directory"; exit 1; }
-    done
+	for file in "$script_dir/bin"/*; do
+		ln -sf "$file" "$BIN_DIR/" || {
+			echo "Failed to update user bin directory"
+			exit 1
+		}
+	done
 }
 
-setup_dotfiles() {
-    create_symlinks
-    install_tmux_plugin_manager
-    install_wallpapers
-    update_git_repositories
-    update_user_bin_directory
-    echo "Dotfiles setup complete."
+function setup_dotfiles() {
+	script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+	create_symlinks
+	install_tmux_plugin_manager
+	install_wallpapers
+	update_git_repositories
+	update_user_bin_directory
+	echo "Dotfiles setup complete."
 }
 
 setup_dotfiles

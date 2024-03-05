4ec6e6a7781fbf6a2f1f3f1c669e2a508df08504
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-05 10:54:51 +0100

    format: using shfmt

--- hypr/scripts/random_wallpaper.sh
+++ hypr/scripts/random_wallpaper.sh
@@ -5,17 +5,17 @@ exclude_dir1=~/.config/wallpapers/.git/
 exclude_dir2=~/.config/wallpapers/anime/.git/
 
 if [ $(ps aux | grep swaybg | grep -v grep | wc -l) -gt 0 ]; then
-    killall swaybg
+	killall swaybg
 fi
 
 swaybg -i "$(find "$wallpapers_dir" -type f -not -path "$exclude_dir1*" -not -path "$exclude_dir2*" | shuf -n1)" -m fit &
 OLD_PID=$!
 
 while true; do
-    sleep 600
-    swaybg -i "$(find "$wallpapers_dir" -type f -not -path "$exclude_dir1*" -not -path "$exclude_dir2*" | shuf -n1)" -m fit &
-    NEXT_PID=$!
-    sleep 5
-    kill $OLD_PID
-    OLD_PID=$NEXT_PID
+	sleep 600
+	swaybg -i "$(find "$wallpapers_dir" -type f -not -path "$exclude_dir1*" -not -path "$exclude_dir2*" | shuf -n1)" -m fit &
+	NEXT_PID=$!
+	sleep 5
+	kill $OLD_PID
+	OLD_PID=$NEXT_PID
 done
--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -5,10 +5,8 @@ TARGET="$HOME/Pictures/Screenshots"
 BETTER_DATE=$(date +%d.%m.%Y-%H:%M:%S)
 
 if [ ! -d "$TARGET" ]; then
-    mkdir -p "$TARGET"
+	mkdir -p "$TARGET"
 fi
-
 
 # grim -g "$(slurp)" "$TARGET/$(date +%s).png" | wl-copy -t image/png
 grim -g "$(slurp)" "$TARGET/$BETTER_DATE.png" && cat "$TARGET/$BETTER_DATE.png" | wl-copy -t image/png
-

57b6de5d54dac3fd6f0b83a4ae6c9b081ac833bf
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-15 08:48:53 +0100

    feat(tmux): handle installs in config itself

--- install.sh
+++ install.sh
@@ -1,24 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-function install_tmux_plugin_manager() {
-	tpm_dir="$HOME/.tmux/plugins/tpm"
-	if [ ! -d "$tpm_dir" ]; then
-		git clone https://github.com/tmux-plugins/tpm "$tpm_dir" || {
-			echo "Failed to install TMUX Plugin Manager"
-			exit 1
-		}
-		echo "TMUX Plugin Manager installed."
-	fi
-}
-
 function setup_dotfiles() {
 	echo "Setting up dotfiles..."
-	local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-	install_tmux_plugin_manager
-
-	echo "Updating dotfiles..."
 	bash "$script_dir/bin/dotupdate"
 
 	echo "Dotfiles setup complete."

315cf8207578ac4dff848cf419588b5b2c40952b
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-08-13 14:08:39 +0200

    change(zsh): update

--- zsh/plugins_config.zsh
+++ zsh/plugins_config.zsh
@@ -1,9 +1,8 @@
 # Download Znap, if it's not there yet.
 [[ -r ~/.zsh/plugins/znap/znap.zsh ]] ||
-git clone --depth 1 -- \
-https://github.com/marlonrichert/zsh-snap.git ~/.zsh/plugins/znap
-source ~/.zsh/plugins/znap/znap.zsh  # Start Znap
-
+	git clone --depth 1 -- \
+		https://github.com/marlonrichert/zsh-snap.git ~/.zsh/plugins/znap
+source ~/.zsh/plugins/znap/znap.zsh # Start Znap
 
 znap source z-shell/zsh-navigation-tools
 znap source Anant-mishra1729/web-search
@@ -11,9 +10,9 @@ znap source zsh-users/zsh-syntax-highlighting
 znap source marlonrichert/zsh-autosuggestions
 znap source arzzen/calc.plugin.zsh
 znap source chisui/zsh-nix-shell
+znap source nix-community/nix-zsh-completions
 
 # custom configs
 ZSH_WEB_SEARCH_ENGINES=(chatgpt "https://chat.openai.com/", mensa "https://www.studentenwerk-dresden.de/mensen/speiseplan/")
 alias chatgpt="web_search chatgpt"
 alias mensa="web_search mensa"
-

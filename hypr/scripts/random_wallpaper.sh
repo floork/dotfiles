ebc3672d1eb5550d2ad359ff49d01c75429ec849
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-08-21 12:25:31 +0200

    feat(wallpaper): set current wallpaper env var

--- hypr/scripts/random_wallpaper.sh
+++ hypr/scripts/random_wallpaper.sh
@@ -7,71 +7,72 @@ INTERVAL=600
 
 # Function to acquire the lock
 function acquire_lock() {
-	if [ -e "$LOCK_FILE" ]; then
-		echo "Another instance is already running. Exiting."
-		exit 1
-	else
-		touch "$LOCK_FILE"
-	fi
+  if [ -e "$LOCK_FILE" ]; then
+    echo "Another instance is already running. Exiting."
+    exit 1
+  else
+    touch "$LOCK_FILE"
+  fi
 }
 
 # Function to release the lock
 function release_lock() {
-	rm -f "$LOCK_FILE"
+  rm -f "$LOCK_FILE"
 }
 
 # Function to get a random wallpaper from the specified directory
 function get_wallpaper() {
-	if [ ! -d "$1" ]; then
-		echo "Error: Wallpapers directory '$1' does not exist."
-		exit 1
-	fi
+  if [ ! -d "$1" ]; then
+    echo "Error: Wallpapers directory '$1' does not exist."
+    exit 1
+  fi
 
-	wallpaper=$(find "$1" -type f -name '*.png' | shuf -n 1)
-	if [ -z "$wallpaper" ]; then
-		echo "Error: No PNG wallpapers found in '$1'."
-		exit 1
-	fi
+  wallpaper=$(find "$1" -type f -name '*.png' | shuf -n 1)
+  if [ -z "$wallpaper" ]; then
+    echo "Error: No PNG wallpapers found in '$1'."
+    exit 1
+  fi
 }
 
 # Function to kill a process
 function kill_process() {
-	if pgrep -x "$1" >/dev/null; then
-		killall "$1"
-	fi
+  if pgrep -x "$1" >/dev/null; then
+    killall "$1"
+  fi
 }
 
 # Function to change the wallpaper
 function change_wallpaper() {
-	local program="$1"
-	if [[ "$program" != "swaybg" && "$program" != "hyprpaper" ]]; then
-		echo "Error: Invalid parameter. Expected 'swaybg' or 'hyprpaper'."
-		return 1
-	fi
+  local program="$1"
+  if [[ "$program" != "swaybg" && "$program" != "hyprpaper" ]]; then
+    echo "Error: Invalid parameter. Expected 'swaybg' or 'hyprpaper'."
+    return 1
+  fi
 
-	kill_process "$program"
+  kill_process "$program"
 
-	case "$program" in
-	swaybg)
-		swaybg -i "$wallpaper" -m fill &
-		;;
-	hyprpaper)
-		sleep 1
-		hyprpaper &
-		;;
-	esac
+  case "$program" in
+  swaybg)
+    swaybg -i "$wallpaper" -m fill &
+    ;;
+  hyprpaper)
+    sleep 1
+    hyprpaper &
+    ;;
+  esac
 }
 
 # Function to set wallpaper
 function set_wallpaper() {
-	get_wallpaper "$WALLPAPERS_DIR"
-	cp "$wallpaper" ~/.cache/current.png
-	change_wallpaper "hyprpaper"
+  get_wallpaper "$WALLPAPERS_DIR"
+  cp "$wallpaper" ~/.cache/current.png
+  export CURRENT_WALLPAPER=$wallpaper
+  change_wallpaper "hyprpaper"
 }
 
 # Function to handle cleanup
 function cleanup() {
-	release_lock
+  release_lock
 }
 
 # Trap cleanup function on exit
@@ -79,15 +80,15 @@ trap cleanup EXIT
 
 # Main function
 function main() {
-	acquire_lock
+  acquire_lock
 
-	set_wallpaper
+  set_wallpaper
 
-	# Set wallpaper at intervals
-	while true; do
-		sleep "$INTERVAL"
-		set_wallpaper
-	done
+  # Set wallpaper at intervals
+  while true; do
+    sleep "$INTERVAL"
+    set_wallpaper
+  done
 }
 
 main
--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -2,71 +2,72 @@
 
 # Function to kill a process if it is running
 function kill_process() {
-	local process=$1
-	if pgrep -x "$process" >/dev/null; then
-		pkill "$process"
-	fi
+  local process=$1
+  if pgrep -x "$process" >/dev/null; then
+    pkill "$process"
+  fi
 }
 
 # Function to kill the random wallpaper script if it is running
 function kill_random_wallpaper() {
-	local pid=$(pgrep -f "hypr/scripts/random_wallpaper.sh")
-	if [ -n "$pid" ]; then
-		kill "$pid"
-	fi
+  local pid=$(pgrep -f "hypr/scripts/random_wallpaper.sh")
+  if [ -n "$pid" ]; then
+    kill "$pid"
+  fi
 }
 
 # Function to apply a wallpaper
 function apply_wallpaper() {
-	local wallpaper=$1
-	local backend=$2
-	kill_process "$backend" # Kill any existing process related to wallpaper_backend
-	kill_random_wallpaper   # Kill the random wallpaper script
+  local wallpaper=$1
+  local backend=$2
+  kill_process "$backend" # Kill any existing process related to wallpaper_backend
+  kill_random_wallpaper   # Kill the random wallpaper script
 
-	cp "$wallpaper" ~/.cache/current.png # Copy the selected wallpaper to cache directory
+  cp "$wallpaper" ~/.cache/current.png # Copy the selected wallpaper to cache directory
+  export CURRENT_WALLPAPER=$wallpaper
 
-	# Check the wallpaper backend and apply accordingly
-	if [ "$backend" == "swaybg" ]; then
-		swaybg -i "$wallpaper" -m fill & # Set wallpaper using swaybg
-		return
-	fi
+  # Check the wallpaper backend and apply accordingly
+  if [ "$backend" == "swaybg" ]; then
+    swaybg -i "$wallpaper" -m fill & # Set wallpaper using swaybg
+    return
+  fi
 
-	if [ "$backend" == "hyprpaper" ]; then
-		sleep 1
-		hyprpaper & # Set wallpaper using hyprpaper
-		return
-	fi
+  if [ "$backend" == "hyprpaper" ]; then
+    sleep 1
+    hyprpaper & # Set wallpaper using hyprpaper
+    return
+  fi
 
-	echo "Backend not found: $2" # Print error if backend not found
+  echo "Backend not found: $2" # Print error if backend not found
 }
 
 # Function to show a menu of wallpapers and apply the selected wallpaper
 function show_wallpaper_menu() {
-	local rofi_cmd="wofi --show dmenu -i -p 'Select a wallpaper: '" # Command to show wallpaper menu
+  local rofi_cmd="wofi --show dmenu -i -p 'Select a wallpaper: '" # Command to show wallpaper menu
 
-	local wallpapers_dir="$HOME/.config/wallpapers"         # Directory containing wallpapers
-	local command="fd . --full-path $wallpapers_dir -e png" # Command to list wallpapers
+  local wallpapers_dir="$HOME/.config/wallpapers"         # Directory containing wallpapers
+  local command="fd . --full-path $wallpapers_dir -e png" # Command to list wallpapers
 
-	local wallpapers=($(eval "$command" | sed "s|$wallpapers_dir/||")) # List of wallpapers with only the part behind wallpapers_dir
+  local wallpapers=($(eval "$command" | sed "s|$wallpapers_dir/||")) # List of wallpapers with only the part behind wallpapers_dir
 
-	local options=$(printf "%s\n" "${wallpapers[@]}") # Options for wallpaper selection menu
+  local options=$(printf "%s\n" "${wallpapers[@]}") # Options for wallpaper selection menu
 
-	local selected_option=$(echo -e "$options" | $rofi_cmd) # Selected wallpaper option
+  local selected_option=$(echo -e "$options" | $rofi_cmd) # Selected wallpaper option
 
-	if [ -n "$selected_option" ]; then
-		selected_option="$wallpapers_dir/$selected_option" # Reconstruct full path
-		apply_wallpaper "$selected_option" "hyprpaper"     # Apply the selected wallpaper
-	else
-		echo "No wallpaper selected."
-	fi
+  if [ -n "$selected_option" ]; then
+    selected_option="$wallpapers_dir/$selected_option" # Reconstruct full path
+    apply_wallpaper "$selected_option" "hyprpaper"     # Apply the selected wallpaper
+  else
+    echo "No wallpaper selected."
+  fi
 }
 
 # Function to check if the wofi menu is open
 function check_wofi_open() {
-	pgrep -x wofi >/dev/null
+  pgrep -x wofi >/dev/null
 }
 
 # Check if the wofi menu is open, if not, show the wallpaper menu
 if ! check_wofi_open; then
-	show_wallpaper_menu
+  show_wallpaper_menu
 fi

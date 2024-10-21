9c44a4eeecf187a3ac6b98641642cbca525fcf17
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-10-21 08:29:38 +0200

    add(zsh/plugins): vi mode

--- zsh/plugins_config.zsh
+++ zsh/plugins_config.zsh
@@ -11,7 +11,14 @@ znap source marlonrichert/zsh-autosuggestions
 znap source arzzen/calc.plugin.zsh
 znap source chisui/zsh-nix-shell
 znap source nix-community/nix-zsh-completions
+znap source jeffreytse/zsh-vi-mode
 
 # custom configs
 ZSH_WEB_SEARCH_ENGINES=(chatgpt "https://chat.openai.com/")
 alias chatgpt="web_search chatgpt"
+
+# Disable the cursor style feature
+export ZVM_CURSOR_STYLE_ENABLED=false
+export ZVM_VI_HIGHLIGHT_FOREGROUND=#1f1d2e
+export ZVM_VI_HIGHLIGHT_BACKGROUND=#f6c177
+export ZVM_VI_HIGHLIGHT_EXTRASTYLE=bold,underline

5915046c2c811bd65095cf4f078bf6684d9433cd
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-02-28 08:40:08 +0100

    change(zsh/plugins): cleanup

--- zsh/plugins_config.zsh
+++ zsh/plugins_config.zsh
@@ -4,22 +4,11 @@
     https://github.com/marlonrichert/zsh-snap.git ~/.zsh/plugins/znap
 source ~/.zsh/plugins/znap/znap.zsh # Start Znap
 
-znap source z-shell/zsh-navigation-tools
-znap source Anant-mishra1729/web-search
 znap source zsh-users/zsh-syntax-highlighting
 znap source marlonrichert/zsh-autosuggestions
-znap source arzzen/calc.plugin.zsh
-znap source chisui/zsh-nix-shell
-znap source nix-community/nix-zsh-completions
-znap source jeffreytse/zsh-vi-mode
+# znap source arzzen/calc.plugin.zsh
+# znap source jeffreytse/zsh-vi-mode
+# znap source chisui/zsh-nix-shell
+# znap source nix-community/nix-zsh-completions
 
-# custom configs
-ZSH_WEB_SEARCH_ENGINES=(chatgpt "https://chat.openai.com/")
-alias chatgpt="web_search chatgpt"
-
-# Disable the cursor style feature
-export ZVM_CURSOR_STYLE_ENABLED=false
-export ZVM_VI_HIGHLIGHT_FOREGROUND=#1f1d2e
-export ZVM_VI_HIGHLIGHT_BACKGROUND=#f6c177
-export ZVM_VI_HIGHLIGHT_EXTRASTYLE=bold,underline
 export ZSH_AUTOSUGGEST_HISTORY_IGNORE=" *"

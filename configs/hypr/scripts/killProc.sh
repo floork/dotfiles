8a357ffd29fc589faaf39c71f8bfe16e0fb0978c
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-05-28 08:18:43 +0200

    fix(hypr): killProc now fully working

--- configs/hypr/scripts/killProc.sh
+++ configs/hypr/scripts/killProc.sh
@@ -1,13 +1,24 @@
 #!/bin/env bash
 
+function app_launch() {
+  # Adjust command for specific launcher
+  if [ "$APP_LAUNCHER" == "wofi" ]; then
+    launcher_cmd="$APP_LAUNCHER --show dmenu -i -p 'Select a process: '"
+  elif [ "$APP_LAUNCHER" == "fuzzel" ]; then
+    launcher_cmd="$APP_LAUNCHER --dmenu -p 'Select a process: '"
+  elif [ "$APP_LAUNCHER" == "walker" ]; then
+    launcher_cmd="$APP_LAUNCHER --dmenu -p 'Select a process: '"
+  fi
+}
+
 function get_pid() {
-  ps --user $(id --user) -o pid,user,cmd | sed '1d' | fuzzel --dmenu | awk '{print $1}'
+  ps --user $(id --user) -o pid,user,comm | sed '1d' | $launcher_cmd | awk '{print $1}'
 }
 
 function kill_menu() {
   local options=("Kill" "Force" "Cancel")
 
-  local selected=$(echo "${options[@]}" | tr ' ' '\n' | fuzzel --dmenu -p "Select an option")
+  local selected=$(echo "${options[@]}" | tr ' ' '\n' | $launcher_cmd)
   echo "$selected"
 }
 
@@ -26,6 +37,7 @@ function kill_process() {
 }
 
 function main() {
+  app_launch
   local pid
   pid=$(get_pid)
 
